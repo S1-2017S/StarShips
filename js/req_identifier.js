20a5fd05376648095c6b9ae2358acc4ce78d884f
DELCOURT <DELCOURT@intechS1.fr>
2017-11-20 14:21:51 +0100

    20/11/2017

--- js/req_identifier.js
+++ js/req_identifier.js
@@ -54,7 +54,7 @@ var trait = function (req, res, query) {
 	} else {
 		// SI IDENTIFICATION OK, ON ENVOIE PAGE ACCUEIL MEMBRE
 
-		page = fs.readFileSync('../html/modele_accueil_membre.html', 'UTF-8');
+		page = fs.readFileSync('../html/placement.html', 'UTF-8');
 
 		marqueurs = {};
 		marqueurs.pseudo = query.pseudo;
