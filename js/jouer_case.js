8954eb2a34c343ceb4363327cd7ff81fb24df367
DELCOURT <DELCOURT@intechS1.fr>
2017-12-18 09:52:46 +0100

    addaz

--- js/jouer_case.js
+++ js/jouer_case.js
@@ -26,8 +26,9 @@ var jouer = function (req, res, query) {
 	var contenu_pseudo;            //
 	var pseudo;                    // pseudo
 
-	var tir = 0;                       
 	marqueur.tir = "";
+
+	var tir = 0;
 
 	//UTILISATION DE LA QUERY
 
@@ -36,13 +37,12 @@ var jouer = function (req, res, query) {
 	requete_J.c = Number(requete_J.c);
 	requete_J.p = query.pseudo;
 
+
 	//LECTURE ETAT PARTIE
-	console.log(requete_J.p);
 	partie = fs.readFileSync("../json/etat_partie.json" , 'utf-8');
 	contenu_partie = JSON.parse(partie);
 	
 	//LECTURE DU PSEUDO
-
 	contenu_pseudo = fs.readFileSync("../json/membres.json" , 'utf-8');
 	pseudo = JSON.parse(contenu_pseudo);
 
@@ -58,127 +58,131 @@ var jouer = function (req, res, query) {
 	//VERIFICATION ECHEC OU REUSSITE DU TIR
 	
 
-	for(var i = 0 ; i < contenu_partie[0].length ; i++) {
-		if(requete_J.c === contenu_partie[0][i].p) {
+	for(var i = 0 ; i < contenu_partie.grille_bot.length ; i++) {
+		if(requete_J.c === contenu_partie.grille_bot[i].p) {
+			contenu_partie.tour.t = 1;
 			tir = 1;
-			contenu_partie[1][contenu_partie[1].length] = contenu_partie[0][i].p 
-		 	contenu_partie[0][i].v = 1;
-			contenu_partie[3][0].s = contenu_partie[3][0].s + 100;
-			if(contenu_partie[0][i].n < 5) {
+			contenu_partie.touche[contenu_partie.touche.length] = contenu_partie.grille_bot[i].p 
+		 	contenu_partie.grille_bot[i].v = 1;
+			contenu_partie.score.s = contenu_partie.score.s + 100;
+			if(contenu_partie.grille_bot[i].n < 5) {
 				tir = 2;
-			    contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-				contenu_partie[3][0].s = contenu_partie[3][0].s + 100;
-			} else if(contenu_partie[0][i].n < 8 && contenu_partie[0][i].n > 4) {
-				if(contenu_partie[0][i].n === contenu_partie[0][i+1].n) {
+			    contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+				contenu_partie.score.s = contenu_partie.score.s + 100;
+			} else if(contenu_partie.grille_bot[i].n < 8 && contenu_partie.grille_bot[i].n > 4) {
+				if(contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+1].n) {
 					plus = true;
-				} else if(contenu_partie[0][i].n === contenu_partie[0][i-1].n) {
+				} else if(contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-1].n) {
 					moins = true;
 				}
 				
-				if(contenu_partie[0][i].v === contenu_partie[0][i+1].v && plus === true) {
+				if(contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+1].v && plus === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+1].p 
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 200;
-				} else if(contenu_partie[0][i].v === contenu_partie[0][i-1].v && moins === true) {
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+1].p 
+					contenu_partie.score.s = contenu_partie.score.s + 200;
+				} else if(contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-1].v && moins === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-1].p 
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 200;
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-1].p 
+					contenu_partie.score.s = contenu_partie.score.s + 200;
 				}
 			
-			} else if(contenu_partie[0][i].n > 7 && contenu_partie[0][i].n < 10) {
+			} else if(contenu_partie.grille_bot[i].n > 7 && contenu_partie.grille_bot[i].n < 10) {
 				
-				if(contenu_partie[0][i].n === contenu_partie[0][i+1].n && contenu_partie[0][i].n === contenu_partie[0][i+2].n) {
+				if(contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+2].n) {
 					plus = true
-				} else if(contenu_partie[0][i].n === contenu_partie[0][i-1].n && contenu_partie[0][i].n === contenu_partie[0][i-2].n) {
+				} else if(contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-2].n) {
 					moins = true;
 				
 
-				} else if(contenu_partie[0][i].n === contenu_partie[0][i-1].n && contenu_partie[0][i].n === contenu_partie[0][i+1].n) {
+				} else if(contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+1].n) {
 					mid = true;
 				}
 				
-				if(contenu_partie[0][i].v === contenu_partie[0][i+1].v && contenu_partie[0][i].v === contenu_partie[0][i+2].v && plus === true) {
+				if(contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+1].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+2].v && plus === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+2].p 
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 300;
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+2].p 
+					contenu_partie.score.s = contenu_partie.score.s + 300;
 				
-				} else if(contenu_partie[0][i].v === contenu_partie[0][i-1].v && contenu_partie[0][i].v === contenu_partie[0][i-2].v && moins === true) {
+				} else if(contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-1].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-2].v && moins === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-2].p 
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 300;
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-2].p 
+					contenu_partie.score.s = contenu_partie.score.s + 300;
 				
-				} else if(contenu_partie[0][i].v === contenu_partie[0][i-1].v && contenu_partie[0][i].v === contenu_partie[0][i+1].v && mid === true) {
+				} else if(contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-1].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+1].v && mid === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-1].p 
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 300;
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-1].p 
+					contenu_partie.score.s = contenu_partie.score.s + 300;
 				
 				}
 		
-			} else if(contenu_partie[0][i].n > 9) {
+			} else if(contenu_partie.grille_bot[i].n > 9) {
 		
-				if(i === 16 &&contenu_partie[0][i].n === contenu_partie[0][i+1].n && contenu_partie[0][i].n === contenu_partie[0][i+2].n && contenu_partie[0][i].n === contenu_partie[0][i+3].n) {
+				if(i === 16 &&contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+2].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+3].n) {
 					plus = true
 
-				} else if(i === 19 && contenu_partie[0][i].n === contenu_partie[0][i-1].n && contenu_partie[0][i].n === contenu_partie[0][i-2].n && contenu_partie[0][i].n === contenu_partie[0][i-3].n) {
+				} else if(i === 19 && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-2].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-3].n) {
 					moins = true;
 
-				} else if(i === 17 && contenu_partie[0][i].n === contenu_partie[0][i-1].n && contenu_partie[0][i].n === contenu_partie[0][i+1].n && contenu_partie[0][i].n === contenu_partie[0][i+2].n) {
+				} else if(i === 17 && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+2].n) {
 					mid_a = true;
 
-				} else if(i === 18 && contenu_partie[0][i].n === contenu_partie[0][i-1].n && contenu_partie[0][i].n === contenu_partie[0][i+1].n && contenu_partie[0][i].n === contenu_partie[0][i-2].n) {
+				} else if(i === 18 && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i+1].n && contenu_partie.grille_bot[i].n === contenu_partie.grille_bot[i-2].n) {
 					mid_b = true;
 				}
 				
-				if(i === 16 && contenu_partie[0][i].v === contenu_partie[0][i+1].v && contenu_partie[0][i].v === contenu_partie[0][i+2].v && contenu_partie[0][i].v === contenu_partie[0][i+3].v && plus === true) {
+				if(i === 16 && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+1].v && contenu_partie[0][i].v === contenu_partie.grille_bot[i+2].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+3].v && plus === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+2].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+3].p 
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 400;
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+2].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+3].p 
+					contenu_partie.score.s = contenu_partie.score.s + 400;
 				
-				} else if(i === 19 && contenu_partie[0][i].v === contenu_partie[0][i-1].v && contenu_partie[0][i].v === contenu_partie[0][i-2].v && contenu_partie[0][i].v === contenu_partie[0][i-3].v && moins === true) {
+				} else if(i === 19 && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-1].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-2].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-3].v && moins === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-2].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-3].p 
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 400;
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-2].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-3].p 
+					contenu_partie.score.s = contenu_partie.score.s + 400;
 				
-				} else if(i === 17 && contenu_partie[0][i].v === contenu_partie[0][i-1].v && contenu_partie[0][i].v === contenu_partie[0][i+1].v && contenu_partie[0][i].v === contenu_partie[0][i+2].v && mid_a === true) {
+				} else if(i === 17 && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-1].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+1].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+2].v && mid_a === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+2].p
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 400;
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+2].p
+					contenu_partie.score.s = contenu_partie.score.s + 400;
 				
-				} else if(i === 18 && contenu_partie[0][i].v === contenu_partie[0][i-1].v && contenu_partie[0][i].v === contenu_partie[0][i+1].v && contenu_partie[0][i].v === contenu_partie[0][i-2].v && mid_b === true) {
+				} else if(i === 18 && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-1].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i+1].v && contenu_partie.grille_bot[i].v === contenu_partie.grille_bot[i-2].v && mid_b === true) {
 					tir = 2;
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i+1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-1].p 
-					contenu_partie[2][contenu_partie[2].length] = contenu_partie[0][i-2].p 
-					contenu_partie[3][0].s = contenu_partie[3][0].s + 400;
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i+1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-1].p 
+					contenu_partie.coule[contenu_partie.coule.length] = contenu_partie.grille_bot[i-2].p 
+					contenu_partie.score.s = contenu_partie.score.s + 400;
 				
 				}
 		
 			}
-		} 
+		
+			
+		}
 	}
 
 	// ATTRIBUTION DES MARQUEURS
 	
-	if(tir === 0 && contenu_partie[4][0].t !== 0) {
-		contenu_partie[3][0].s = contenu_partie[3][0].s - 50;
+	
+	if(tir === 0 && contenu_partie.tour.t === 0) {
+		contenu_partie.score.s = contenu_partie.score.s - 50;
 		marqueur.tir = "raté"
 	} else if(tir === 1) {
 		marqueur.tir = "touché"
@@ -186,28 +190,37 @@ var jouer = function (req, res, query) {
 		marqueur.tir = "détruit"
 	}
 
-	marqueur.score = contenu_partie[3][0].s;
-	contenu_partie[4][0].t = contenu_partie[4][0].t + 1;
+	marqueur.score = contenu_partie.score.s;
 
-	for(var m = 0 ; m < contenu_partie[1].length ; m++) {
-		marqueur[contenu_partie[1][m]] = "<img src='../img/vert.png'></a></td>";
+	for(var m = 0 ; m < contenu_partie.touche.length ; m++) {
+		marqueur[contenu_partie.touche[m]] = "<img src='../img/vert.png'></a></td>";
 	}
 	
-	for(var n = 0 ; n < contenu_partie[2].length ; n++) {
-		marqueur[contenu_partie[2][n]] = "<img src='../img/rouge.png'></a></td>";
+	for(var n = 0 ; n < contenu_partie.coule.length ; n++) {
+		marqueur[contenu_partie.coule[n]] = "<img src='../img/rouge.png'></a></td>";
 	}
 	
 	
 	partie = JSON.stringify(contenu_partie);
 	fs.writeFileSync("../json/etat_partie.json",partie,'utf-8');
 	
+	
 
 	// AFFICHAGE DE LA PAGE DE JEU
 
+	if(contenu_partie.tour.t === 0) {
+	
+	page = fs.readFileSync('../html/joueur_passif.html', 'utf-8');
+	contenu_partie.tour.t = 1;
+	
+
+	} else if(contenu_partie.tour.t === 1) {
+	
 	page = fs.readFileSync('../html/joueur_actif.html', 'utf-8');
 	page = page.supplant(marqueur);
 
-
+	}
+	
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(page);
 	res.end();
